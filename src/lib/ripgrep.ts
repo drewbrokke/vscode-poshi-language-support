489d097fd5f051810619e449ed98f527358cbbc1
drewbrokke <drew.brokke@liferay.com>
2022-03-10 10:55:46 -0600

    adds method to return match objects based on ripgrep's '--json' output. This should handle different file path formats in different OS's

--- src/lib/ripgrep.ts
+++ src/lib/ripgrep.ts
@@ -3,14 +3,20 @@ import { spawn } from 'child_process';
 import { rgPath } from '@vscode/ripgrep';
 
 export interface RipgrepArgs {
-	args: string[];
+	args?: string[];
 	globs?: string[];
 	paths: string[];
 	search: string;
 }
 
+export interface RipgrepMatch {
+	filepath: string;
+	lineNumber: number;
+	columnNumber: number;
+}
+
 export async function ripgrep(ripgrepArgs: RipgrepArgs): Promise<string[]> {
-	const { args, globs, paths, search } = ripgrepArgs;
+	const { args = [], globs, paths, search } = ripgrepArgs;
 
 	const totalArgs = [...args];
 
@@ -31,4 +37,35 @@ export async function ripgrep(ripgrepArgs: RipgrepArgs): Promise<string[]> {
 	}
 
 	return text.trim().split('\n');
+}
+
+export async function ripgrepMatches(
+	ripgrepArgs: RipgrepArgs,
+): Promise<RipgrepMatch[]> {
+	const { args = [] } = ripgrepArgs;
+	const lines = await ripgrep({
+		...ripgrepArgs,
+		args: [...args, '--json'],
+	});
+
+	return (
+		lines
+			.map((line) => JSON.parse(line))
+			// See https://docs.rs/grep-printer/0.1.6/grep_printer/struct.JSON.html#overview
+			.filter((obj) => obj.type === 'match')
+			.map((obj) => obj.data)
+			.reduce((acc, obj) => {
+				// See https://docs.rs/grep-printer/0.1.6/grep_printer/struct.JSON.html#message-match
+
+				for (const submatch of obj.submatches) {
+					acc.push({
+						filepath: obj.path.text,
+						lineNumber: obj.line_number,
+						columnNumber: submatch.start,
+					});
+				}
+
+				return acc;
+			}, [])
+	);
 }
